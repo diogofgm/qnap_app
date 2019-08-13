3e836215c1e7b0d8f0cf335c3d0af7e076f651b0
Diogo Silva <diogo.silva@devbusters.com>
2019-08-14 00:30:53 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2019, Diogo Silva'
 author = u'Diogo Silva'
 
 # The short X.Y version
-version = u''
+version = u'0.1'
 # The full version, including alpha/beta/rc tags
-release = u'2019'
+release = u'0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -75,7 +75,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
